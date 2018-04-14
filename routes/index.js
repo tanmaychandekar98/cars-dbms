c5098706ad384ac59a6c340c8d9e8d0884fe39c8
Tanmay Chandekar <tanmaychandekar98@gmail.com>
2018-04-14 21:00:50 +0530

    Added bootstrap , navbar

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var fs = require('fs');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Cars' });
 });
+
+/*router.get('/signup',function(req,res){
+
+});
+*/
 
 module.exports = router;
